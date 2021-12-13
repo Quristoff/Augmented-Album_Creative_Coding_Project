2373f35a819f4155e698d4389e0b8651b222ef72
Quristoff <85057165+Quristoff@users.noreply.github.com>
2021-12-12 21:21:03 -0500

    Update WORLD.js

--- WORLD/WORLD.js
+++ WORLD/WORLD.js
@@ -16,7 +16,7 @@ let cameraOff = false;
 
 
 function setup(){
-    cav = createCanvas(windowWidth,windowHeight);
+    cav = createCanvas(windowWidth*1.2,windowHeight*1.2);
     frameRate(25);
 
     // the handpose
